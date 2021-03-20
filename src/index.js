a727857fd454f2e2238feb49841783b0f50f4cd0
Sekib Omazic <sekib.omazic-ext@rio.cloud>
2021-03-20 18:29:03 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+import usersRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+const page = `
+<head>
+  <title>Blue-Green deployment</title>
+</head>
+
+<body style="background-color: cornflowerblue;">
+  <h1 style="color: white; text-align: center;">
+    Hello from AWS Fargate
+  </h1>
+</body>
+`
+
+app.get('/', (_req, res) => {
+  res.setHeader('Content-type', 'text/html')
+  return res.send(page)
+})
+
+app.use('/user', usersRoutes);
+app.get('/health', (_req, res) => res.send('Healthy!'));
+app.all('*', (_req, res) => res.send('Ooops, no such route'));
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
